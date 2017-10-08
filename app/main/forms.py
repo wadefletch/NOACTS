b2b5f904294fedd74ea1bd5259a23b359caaa89d
wadefletcher <wbfletch@gmail.com>
2017-10-08 03:15:36 -0700

    update

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import SelectField, StringField
+from wtforms import SelectField, StringField, PasswordField, validators
 from wtforms.validators import DataRequired
 
 
@@ -9,3 +9,6 @@ class LineEntryForm(FlaskForm):
 class StartForm(FlaskForm):
     script = SelectField('Ceremony', choices=[('pre', 'Pre-Ordeal')])
     role = SelectField('Role', choices = [('Kichkinet', 'Kichkinet')]) #, ('Meteu', 'Meteu'), ('Nutiket', 'Nutiket'), ('Allowat Sakima', 'Allowat Sakima')])
+    admonition = PasswordField('Admonition', validators=[
+        validators.Regexp('ahoalton', message='You have administered the admonition incorrectly.')
+    ])
--- app/main/views.py
+++ app/main/views.py
@@ -4,6 +4,7 @@ from .forms import *
 import csv
 import os
 import json
+import md5
 
 @main.route('/')
 def index():
@@ -12,7 +13,8 @@ def index():
 
 @main.route('/scriptreader', methods=['GET', 'POST'])
 def script():
-    session['line'] = 0 # REMOVE FOR PRODUCTION!!!!!
+    if not session['authenticated']:
+        return redirect(url_for('main.start'))
     form = LineEntryForm()
     if form.validate_on_submit():
         form.line.data = ''
@@ -26,11 +28,14 @@ def start():
     if form.validate_on_submit():
         session['role'] = form.role.data
         session['script'] = form.script.data
+        session['authenticated'] = True
         return redirect(url_for('main.script'))
     return render_template('start.html', form=form)
 
 @main.route('/line/<index>')
 def line(index):
+    if not session['authenticated']:
+        return redirect(url_for('main.start'))
     lines = []
     with open(os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'static/preordeal.tsv')) as f:
         reader = csv.reader(f, delimiter='\t', )
